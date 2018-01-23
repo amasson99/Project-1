cf35e7b42cb2b40fa73fdffc902d1d3b3844569e
Alex Masson <amasson99@gmail.com>
2018-01-23 10:07:27 -0600

    initial changes

--- Proj1_Part1.java
+++ Proj1_Part1.java
@@ -4,12 +4,12 @@
 *
 * This program is an appliance store is offering 3% financing for 12 months on any purchase .
 *******************************************************************/
-import java.util.*;
-// Add import line here to use the Scanner class
-public class Lab1 {
+import java.util.*;// Add import line here to use the Scanner class
+public class Proj1_Part1 {
 public static void main(String[] args)
 {
-	Scanner in = new Scanner (System.in);
-// Declare a Scanner object here to be used for input
+	Scanner in = new Scanner (System.in);// Declare a Scanner object here to be used for input
+
+  in.close();//close the scanner
 } // end main
 } // end class Lab1
