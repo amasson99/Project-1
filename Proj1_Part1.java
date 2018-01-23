12a6706816a48bca2d7d996c728cbf40f2bf9715
Alex Masson <amasson99@gmail.com>
2018-01-23 12:40:28 -0600

    Finished product

--- Proj1_Part1.java
+++ Proj1_Part1.java
@@ -14,6 +14,7 @@ public class Proj1_Part1 {
 		String input = in.nextLine(); //read the user input as a String
 		String item; //item user purchases
 		double financing = .03; //constant finance rate
+		double financingprice;
 		double purchaseprice; //what the purchasing price of the item is
 		double months = 12;//constant months for financing calculations
 		double monthlypayment; //what the monthly payment will be
@@ -22,7 +23,16 @@ public class Proj1_Part1 {
 
 		System.out.print("Enter the item to be purchased: ");
 			item = in.nextLine();
-		System.out.println("The item you just said is: " + item);
+
+		System.out.print("Enter the amount of the purchase:");
+			input = in.nextLine();
+			purchaseprice = Double.parseDouble(input);
+			financingprice = purchaseprice * financing;
+			monthlypayment = (purchaseprice + financingprice)/months;
+
+		System.out.printf("Your monthly payment is $%.2f\n", monthlypayment);
+		System.out.printf("Your total payment is $%.2f\n", purchaseprice + financingprice);
+		System.out.printf("Amount paid in financing is $%.2f\n", financingprice);
 
 
   	in.close();//close the scanner
