2463e0a4f64a62b8896ffb24f5380903e861ab8c
Alex Masson <amasson99@gmail.com>
2018-01-23 12:19:36 -0600

    Fixed the fix

--- Proj1_Part1.java
+++ Proj1_Part1.java
@@ -5,22 +5,24 @@
 * This program is an appliance store is offering 3% financing for 12 months on any purchase .
 *******************************************************************/
 import java.util.*;// Add import line here to use the Scanner class
-public class Proj1_Part1 {
-public static void main(String[] args)
-{
-	Scanner in = new Scanner (System.in);// Declare a Scanner object here to be used for input
-	String input = in.nextline(); //read the user input as a String
-	String item; //item user purchases
-	double financing = .03; //constant finance rate
-	double purchaseprice; //what the purchasing price of the item is
-	double months = 12;//constant months for financing calculations
-	double monthlypayment; //what the monthly payment will be
-	double totalpayment; //what the total payment will be
-	double financingpayment; //what the financing payment for each month will be
-	System.out.print("Enter the item to be purchased: ");
-		input = in.nextline();
-		item = String.format("Enter the item to be purchased: " + item);
 
-  in.close();//close the scanner
-} // end main
+public class Proj1_Part1 {
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner (System.in);// Declare a Scanner object here to be used for input
+
+		String input = in.nextLine(); //read the user input as a String
+		String item; //item user purchases
+		double financing = .03; //constant finance rate
+		double purchaseprice; //what the purchasing price of the item is
+		double months = 12;//constant months for financing calculations
+		double monthlypayment; //what the monthly payment will be
+		double totalpayment; //what the total payment will be
+		double financingpayment; //what the financing payment for each month will be
+
+		System.out.print("Enter the item to be purchased: ");
+			input = in.nextLine();
+
+  	in.close();//close the scanner
+	} // end main
 } // end class Lab1
