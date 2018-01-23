71ecdbe623ac1e3a08219d90db2ad6f81fc71418
Alex Masson <amasson99@gmail.com>
2018-01-23 10:03:23 -0600

    Initial commit

---
+++ Proj1_Part1.java
@@ -0,0 +1,15 @@
+/*******************************************************************
+* Lab1.java
+* <Alex Masson / Section B/ Thursday 4:30 - 6:30>
+*
+* This program is an appliance store is offering 3% financing for 12 months on any purchase .
+*******************************************************************/
+import java.util.*;
+// Add import line here to use the Scanner class
+public class Lab1 {
+public static void main(String[] args)
+{
+	Scanner in = new Scanner (System.in);
+// Declare a Scanner object here to be used for input
+} // end main
+} // end class Lab1
